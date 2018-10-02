6b19af718c7c16816e8eee3b426c6788d30190fa
Kate Rentschler <nkpe@outlook.com>
2018-10-02 23:30:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,12 @@ var chocolateBars=[
 ]
 var array = new Array();
 function addElementToBeginningOfArray(array, element){
+<<<<<<< HEAD
 array= [element,...array];
 return array;
+=======
+return [element,...array];
+>>>>>>> 5fcda697dcce36a5b24685783005ebc7752b02f4
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
@@ -16,8 +20,12 @@ return array;
 }
 
 function addElementToEndOfArray(array, element){
+<<<<<<< HEAD
   array= [...array, element];
   return array;
+=======
+  return [...array, element];
+>>>>>>> 5fcda697dcce36a5b24685783005ebc7752b02f4
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
@@ -33,6 +41,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array;
 }
+<<<<<<< HEAD
 
 function removeElementFromBeginningOfArray(array){
   array= array.slice(1);
@@ -48,3 +57,5 @@ function removeElementFromEndOfArray(array){
   array=array.slice(0, array.length-1);
   return array;
 }
+=======
+>>>>>>> 5fcda697dcce36a5b24685783005ebc7752b02f4
